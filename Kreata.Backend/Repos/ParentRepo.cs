96dffeb56ac1731af2efab1f6c197e58abff898b
BorosBalazsLaszlo <c9borbal@vasvari.org>
2024-11-06 19:29:06 +0100

    teacher repo kesz

---
+++ Kreata.Backend/Repos/ParentRepo.cs
@@ -0,0 +1,48 @@
+using Kreata.Backend.Context;
+using Kreata.Backend.Datas.Entities;
+using Kreata.Backend.Repos;
+using Kreta.Backend.Datas.REsponses;
+using Microsoft.EntityFrameworkCore;
+
+public class StudentRepo : IStudentRepo
+{
+    private readonly KretaInMemoryContext _dbContext;
+
+    public StudentRepo(KretaInMemoryContext dbContext)
+    {
+
+        _dbContext = dbContext;
+    }
+
+    public async Task<Student?> GetBy(Guid id)
+    {
+
+        return await _dbContext.Students.FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task<List<Student>> GetAll()
+    {
+
+        return await _dbContext.Students.ToListAsync();
+    }
+
+    public async Task<ControllerResponse> UpdateStudentAsync(Student student)
+    {
+        ControllerResponse response = new ControllerResponse();
+        _dbContext.ChangeTracker.Clear();
+        _dbContext.Entry(student).State = EntityState.Modified;
+        try
+        {
+
+            await _dbContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            response.AddNewError(e.Message);
+            response.AddNewError($"{nameof(StudentRepo)} osztály, {nameof(UpdateStudentAsync)} hiba keletkezett");
+            response.AddNewError($"{student} frissítés sikertelen!");
+        }
+        return response;
+    }
+}
+}
--- Kreata.Backend/Repos/TeacherRepo.cs
+++ Kreata.Backend/Repos/TeacherRepo.cs
@@ -2,6 +2,7 @@
 using Kreata.Backend.Context;
 using Kreata.Backend.Datas.Entities;
 using Kreata.Backend.Repos;
+using Kreta.Backend.Datas.REsponses;
 using Microsoft.EntityFrameworkCore;
 
 public class TeacherRepo : ITeacherRepo
@@ -19,6 +20,24 @@ public class TeacherRepo : ITeacherRepo
 
     public async Task<Teacher?> GetBy(Guid id)
     {
-        return await _dbContext.Teachers.FirstOrDefaultAsync(s => s.Id == id);
+        return await _dbContext.Teachers.FirstOrDefaultAsync(t => t.Id == id);
+    }
+    public async Task<ControllerResponse> UpdateTeacherAsync(Teacher teacher)
+    {
+        ControllerResponse response = new ControllerResponse();
+        _dbContext.ChangeTracker.Clear();
+        _dbContext.Entry(teacher).State = EntityState.Modified;
+        try
+        {
+
+            await _dbContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            response.AddNewError(e.Message);
+            response.AddNewError($"{nameof(TeacherRepo)} osztály, {nameof(UpdateTeacherAsync)} hiba keletkezett");
+            response.AddNewError($"{teacher} frissítés sikertelen!");
+        }
+        return response;
     }
 }
