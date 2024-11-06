9f82f40d65a4fafb00bd8882b2de82d9d8e5fbf2
BorosBalazsLaszlo <c9borbal@vasvari.org>
2024-11-06 19:37:53 +0100

    apro hiba javitva

--- Kreata.Backend/Repos/IParentRepo.cs
+++ Kreata.Backend/Repos/IParentRepo.cs
@@ -5,8 +5,8 @@ namespace Kreata.Backend.Repos
 {
     public interface IParentRepo
     {
-        Task<List<Student>> GetAll();
-        Task<Student?> GetBy(Guid id);
+        Task<List<Parent>> GetAll();
+        Task<Parent?> GetBy(Guid id);
         Task<ControllerResponse> UpdateParentAsync(Parent Parent;
     }
 }
